7a3215589c5d46c030be4ffe449dd84eef35e172
mikkiDev <mikkimail.games@gmail.com>
2025-09-12 14:32:03 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -5,9 +8,25 @@ plugins {
     id("kotlin-kapt")
 }
 
+
 android {
     namespace = "com.example.mweight"
     compileSdk = 36
+
+    val keystorePropertiesFile = rootProject.file("keystore.properties")
+    val keystoreProperties = Properties()
+    if (keystorePropertiesFile.exists()) {
+        keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile       = file(keystoreProperties.getProperty("storeFile"))
+            storePassword   = keystoreProperties.getProperty("storePassword")
+            keyAlias        = keystoreProperties.getProperty("keyAlias")
+            keyPassword     = keystoreProperties.getProperty("keyPassword")
+        }
+    }
 
     defaultConfig {
         applicationId = "com.example.mweight"
@@ -21,7 +40,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            signingConfigs.getByName("release")
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
