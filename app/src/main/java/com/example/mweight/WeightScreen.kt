862ac9afb5d0872d69fa0b9ea705958792b6dffe
mikkiDev <mikkimail.games@gmail.com>
2025-09-15 14:13:57 +0200

    notification button

--- app/src/main/java/com/example/mweight/WeightScreen.kt
+++ app/src/main/java/com/example/mweight/WeightScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
@@ -34,6 +35,7 @@ import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SwipeToDismissBox
@@ -86,10 +88,20 @@ fun WeightScreen(weightViewModel: WeightViewModel = viewModel()) {
     Scaffold(
         topBar = {
             Column {
+
                 TopAppBar(
                     title = { Text(text = "mWeight") },
-                    scrollBehavior = topBarScrollBehavior
+                    scrollBehavior = topBarScrollBehavior,
+                    actions = {
+                        IconButton(onClick = { null }) {
+                            Icon(
+                                imageVector = Icons.Default.Notifications,
+                                contentDescription = null
+                            )
+                        }
+                    }
                 )
+
                 HorizontalDivider(
                     thickness = 0.5.dp,
                     color = MaterialTheme.colorScheme.secondary,
